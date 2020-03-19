6b29081ce4e62b5297e80e1c0c461c723144bacc
locokict <locokict@gmail.com>
2020-03-18 23:51:17 -0400

    actualizacion

---
+++ src/pages/nosotros/Nosotros.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+class Nosotros extends React.Component{
+
+    
+}
+
+export default Nosotros;
---
+++ src/pages/servicios/Servicios.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+class Servicios extends React.Component{
+
+}
+
+export default Servicios;
