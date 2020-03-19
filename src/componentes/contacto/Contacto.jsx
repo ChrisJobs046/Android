9055e1975e09f00d334dca705f7a3106701f55e4
locokict <locokict@gmail.com>
2020-03-19 00:02:54 -0400

    Bro ahora si 😂

---
+++ src/componentes/contacto/Contacto.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import Menu from '../menu/Menu';
+import Formulario from './formulario/Formulario';
+import Footer from '../footer/Footer';
+
+
+class Contacto extends React.Component {
+
+	render() {
+
+		return(
+
+			<>
+
+			<Menu />
+
+			<main role="main" className="flex-shrink-0 mt-5">
+
+				<div className="container">
+
+					<h1 className="mb-5">Contacto</h1>
+
+            		<div className="row">
+
+            			<div className="col-md-6">
+		        
+							<Formulario />
+
+						</div>
+
+						<div className="col-md-6">
+		        
+							
+
+						</div>
+
+					</div>
+				</div>	
+
+	  		</main>
+
+	  		<Footer />
+
+	  		</>
+
+		)
+
+	}
+
+}
+
+export default Contacto;
